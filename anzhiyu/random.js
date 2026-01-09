000aa354dba95d0829300fdb9d16e9e7f5118e34
qiusurui <17337890960@163.com>
2026-01-09 15:07:30 +0800

    Site updated: 2026-01-09 15:07:30

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["c551739d/","f873d6e0/","72cfe8e6/","395bbd0/","67edc204/","505985a3/","43934f6d/","830fe511/","9e096d84/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };

91271eba96b30574ae895761c01be7417899c445
qiusurui <17337890960@163.com>
2026-01-12 09:56:34 +0800

    Site updated: 2026-01-12 09:56:34

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["c551739d/","f873d6e0/","67edc204/","72cfe8e6/","395bbd0/","505985a3/","43934f6d/","9e096d84/","340eec7/","830fe511/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };

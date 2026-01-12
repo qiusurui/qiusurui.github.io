ce9d2e08252d184da482bcc28a57b54d2bc0e0a5
qiusurui <17337890960@163.com>
2026-01-12 10:19:40 +0800

    Site updated: 2026-01-12 10:19:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["c551739d/","f873d6e0/","67edc204/","72cfe8e6/","395bbd0/","505985a3/","43934f6d/","9e096d84/","340eec7/","830fe511/"];function toRandomPost(){
+var posts=["67edc204/","c551739d/","f873d6e0/","72cfe8e6/","505985a3/","395bbd0/","43934f6d/","9e096d84/","830fe511/","340eec7/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
